81d7c7049dcbcb580328f4459bcdb02427791b25
James <145038163+SteelSinter@users.noreply.github.com>
2023-11-27 14:02:42 -0700

    Add files via upload

--- Book Examples/Test.java
+++ Book Examples/Test.java
@@ -1,12 +1,18 @@
 package pack;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.scene.shape.Circle;
 import javafx.scene.paint.Color;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
 
 public class Test extends Application{
 	
@@ -25,21 +31,37 @@ public class Test extends Application{
 				Color.AQUA
 		};
 		
-		Button btOK = new Button("OK");
+		Button button = new Button("Press this button");
 		Circle circle = new Circle();
 		StackPane pane = new StackPane();
+		Font font = new Font("Comic Sans MS", 10);
+		Image image = new Image("https://i.etsystatic.com/23587835/r/il/0c6e6d/3690885284/il_fullxfull.3690885284_4uhn.jpg");
+		ImageView imageView = new ImageView(image);
+		//BackgroundFill bFill = ;
+		
+		button.rotateProperty().bind(pane.widthProperty());
+		button.scaleXProperty().bind(pane.widthProperty().divide(100));
+		button.scaleYProperty().bind(pane.widthProperty().divide(100));
+		button.setFont(font);
+		
+		imageView.scaleXProperty().bind(pane.widthProperty().divide(1200));
+		imageView.scaleYProperty().bind(pane.heightProperty().divide(1200));
+		
+		pane.rotateProperty().bind(button.rotateProperty());
+		//pane.setStyle("-fx-background-color: #" + Color.color(pane.getWidth() / 10, pane.getHeight() / 10, 0).hashCode());
 		
 		circle.centerXProperty().bind(pane.widthProperty().divide(2));
 		circle.centerXProperty().bind(pane.widthProperty().divide(2));
 		circle.radiusProperty().bind(pane.widthProperty().divide(10));
-		circle.setStroke(Color.BLACK);
+		circle.setStroke(Color.RED);
 		circle.setFill(Color.BLUE);
-		//circle.strokeProperty().bind();
+		circle.setStyle("-fx-stroke: orange; -fx-fill: red;");
 		
-		pane.getChildren().add(btOK);
-		pane.getChildren().add(circle);
+		pane.getChildren().add(button);
+		//pane.getChildren().add(circle);
+		pane.getChildren().add(imageView);
 		
-		Scene scene = new Scene(pane, 200, 200);
+		Scene scene = new Scene(pane, 180, 200);
 		
 		primaryStage.setTitle("ALERT!");
 		primaryStage.setScene(scene);
