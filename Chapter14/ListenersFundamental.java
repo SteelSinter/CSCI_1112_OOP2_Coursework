c8465b98375dc591821c0e3a85816560fa66753f
James <145038163+SteelSinter@users.noreply.github.com>
2023-12-04 13:52:12 -0700

    Add files via upload

--- Chapter14/ListenersFundamental.java
+++ Chapter14/ListenersFundamental.java
@@ -1,10 +1,15 @@
 package pack;
 import javafx.application.Application;
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
@@ -12,25 +17,29 @@ import javafx.stage.Stage;
 public class ListenersFundamental extends Application{
 	@Override
 	public void start(Stage primaryStage) {
-		Circle ball = new Circle(20);
-		StackPane ballPane = new StackPane(ball);
+		
+		BallPane ballPane = new BallPane();
+		ballPane.setMaxWidth(ballPane.getBall().getRadius() * 2);
 		BorderPane pane = new BorderPane();
 		Button btUp = new Button("UP");
 		Button btDown = new Button("DOWN");
 		Button btLeft = new Button("LEFT");
 		Button btRight = new Button("RIGHT");
+		
+		pane.setCenter(ballPane);
 		pane.setTop(btUp);
 		pane.setBottom(btDown);
 		pane.setRight(btRight);
 		pane.setLeft(btLeft);
-		pane.setCenter(ballPane);
 		pane.setPadding(new Insets(10));
 		pane.setAlignment(btUp, Pos.TOP_CENTER);
 		pane.setAlignment(btDown, Pos.CENTER);
 		pane.setAlignment(btRight, Pos.CENTER_RIGHT);
 		pane.setAlignment(btLeft, Pos.CENTER_LEFT);
-		
-		btUp.setOnAction
+
+		btUp.setOnAction(e -> {
+			ballPane.moveUp();
+		});
 		
 		Scene scene = new Scene(pane, 400, 400);
 		primaryStage.setScene(scene);
@@ -41,6 +50,28 @@ public class ListenersFundamental extends Application{
 	public static void main(String[] args) {
 		Application.launch(args);
 
+	}
+	
+	public class BallPane extends StackPane {
+		private int movementInterval = 10;
+		private Circle ball;
+		BallPane() {
+			ball = new Circle(30);
+			getChildren().add(ball);
+			
+		}
+		
+		public Circle getBall() {
+			return ball;
+		}
+		
+		public void moveUp() {
+			setTranslateY(movementInterval);
+		}
+		
+		public void moveDown() {
+			setTranslateY(movementInterval);
+		}
 	}
 
 }
