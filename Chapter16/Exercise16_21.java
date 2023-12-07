41918c0ae3efe8b24d0d15c2a4cbdf5283d4268d
James <145038163+SteelSinter@users.noreply.github.com>
2023-12-07 13:00:18 -0700

    Add files via upload

--- Chapter16/Exercise16_21.java
+++ Chapter16/Exercise16_21.java
@@ -1,16 +1,25 @@
 package pack;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class Exercise16_21 extends Application {
 	@Override
 	public void start(Stage stage) {
+		Media music = new Media("https://liveexample.pearsoncmg.com/common/audio/anthem/anthem0.mp3");
+		MediaPlayer mediaPlayer = new MediaPlayer(music);
 		TextField field = new TextField();
 		field.setMinWidth(100);
 		field.setMinHeight(100);
@@ -21,8 +30,22 @@ public class Exercise16_21 extends Application {
 		
 		Scene scene = new Scene(pane, 150, 100);
 		
+		EventHandler<ActionEvent> countdownHandler = e -> {
+			try {
+				field.setText(String.valueOf(Integer.parseInt(field.getText()) - 1));
+			}
+			catch (Exception ex) {}
+		};
+		
+		Timeline countdown = new Timeline(new KeyFrame(Duration.millis(1000), countdownHandler));
+		countdown.setOnFinished(e -> mediaPlayer.play());
+		
 		field.setOnAction(e -> {
-			
+			try {
+				countdown.setCycleCount(Integer.parseInt(field.getText()));
+				countdown.play();
+			}
+			catch (Exception ex) {}
 		});
 		
 		stage.setScene(scene);
@@ -34,5 +57,4 @@ public class Exercise16_21 extends Application {
 		Application.launch();
 
 	}
-
 }
