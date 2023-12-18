7ef5f5379e9fb1b229f4e00b2b81f09937c941f8
James <145038163+SteelSinter@users.noreply.github.com>
2023-12-18 10:45:50 -0700

    Add files via upload

---
+++ Book Examples/HTree.java
@@ -0,0 +1,15 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+public class HTree extends Application{
+
+	@Override
+	public void start(Stage mainStage) {
+		
+	}
+	
+	public static void main(String[] args) {
+		Application.launch(args);
+
+	}
+
+}
