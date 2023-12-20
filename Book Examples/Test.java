6ec36d907eb55c23a6258db9885a91dc34ce4e21
James <145038163+SteelSinter@users.noreply.github.com>
2023-12-20 08:44:31 -0700

    Add files via upload

--- Book Examples/Test.java
+++ Book Examples/Test.java
@@ -1,77 +1,80 @@
-package pack;
 
-import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.StackPane;
-import javafx.scene.shape.Circle;
-import javafx.scene.paint.Color;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.text.Font;
-import javafx.stage.Stage;
-
-public class Test extends Application{
-	
-	@Override
-	public void start(Stage primaryStage) {
-		Color[] colors = {
-				Color.RED,
-				Color.BLUE,
-				Color.YELLOW,
-				Color.GREEN,
-				Color.ORANGE,
-				Color.PINK,
-				Color.MAGENTA,
-				Color.AQUA,
-				Color.GOLD,
-				Color.AQUA
-		};
-		
-		Button button = new Button("Press this button");
-		Circle circle = new Circle();
-		StackPane pane = new StackPane();
-		Font font = new Font("Comic Sans MS", 10);
-		Image image = new Image("https://i.etsystatic.com/23587835/r/il/0c6e6d/3690885284/il_fullxfull.3690885284_4uhn.jpg");
-		ImageView imageView = new ImageView(image);
-		//BackgroundFill bFill = ;
-		
-		button.rotateProperty().bind(pane.widthProperty());
-		button.scaleXProperty().bind(pane.widthProperty().divide(100));
-		button.scaleYProperty().bind(pane.widthProperty().divide(100));
-		button.setFont(font);
-		
-		imageView.scaleXProperty().bind(pane.widthProperty().divide(1200));
-		imageView.scaleYProperty().bind(pane.heightProperty().divide(1200));
-		
-		pane.rotateProperty().bind(button.rotateProperty());
-		//pane.setStyle("-fx-background-color: #" + Color.color(pane.getWidth() / 10, pane.getHeight() / 10, 0).hashCode());
-		
-		circle.centerXProperty().bind(pane.widthProperty().divide(2));
-		circle.centerXProperty().bind(pane.widthProperty().divide(2));
-		circle.radiusProperty().bind(pane.widthProperty().divide(10));
-		circle.setStroke(Color.RED);
-		circle.setFill(Color.BLUE);
-		circle.setStyle("-fx-stroke: orange; -fx-fill: red;");
-		
-		pane.getChildren().add(button);
-		//pane.getChildren().add(circle);
-		pane.getChildren().add(imageView);
-		
-		Scene scene = new Scene(pane, 180, 200);
-		
-		primaryStage.setTitle("ALERT!");
-		primaryStage.setScene(scene);
-		primaryStage.show();
-		
-	}
+public class Test {
 
 	public static void main(String[] args) {
-		Application.launch(args);
+		//System.out.println(f1(3));
+		//System.out.println(f2(3, 0));
+		//System.out.println(xfunction(6));
+		//System.out.println(m(5));
+		//System.out.println(fib(5));
+		//moveDisks(4, 'A', 'B', 'C');
+		//int[] x = {1, 2, 3, 4, 5};
+		//yMethod(x, 5);
+		
+		System.out.println(zMethod(4));
 
+	}
+	
+	public static int f1(int n) {
+		if (n == 0)
+			return 0;
+		else {
+			return n + f1(n-1);
+		}
+	}
+	
+	public static int f2(int n, int result) {
+		if (n == 0)
+			return result;
+		else
+			return f2(n - 1, n + result);
+	}
+	
+	public static int xfunction(int n) {
+		if (n <= 1)
+			return 1;
+		else
+			return n + xfunction(n - 2);
+	}
+	
+	public static int m(int value) {
+		if (value >= 0)
+			return 5 * m(value - 2);
+		else
+			return 1;
+	}
+	
+	public static long fib(long index) {
+		System.out.println("fib");
+		if (index == 0 )
+			return 0;
+		else if (index == 1)
+			return 1;
+		else
+			return fib(index - 1) + fib(index - 2);
+	}
+	
+	public static void moveDisks(int n, char fromTower, char toTower, char auxTower) {
+		System.out.println("moveDisks invoked");
+		if (n == 1)
+			System.out.println("Move disk " + n + " from " + fromTower + " to " + toTower);
+		else {
+			moveDisks(n - 1, fromTower, auxTower, toTower);
+			System.out.println("Move disk " + n + " from " + fromTower + " to " + toTower);
+			moveDisks(n - 1, auxTower, toTower, fromTower);
+		}
+	}
+	
+	public static void yMethod(int[] x, int length) {
+		System.out.print(" " + x[length - 1]);
+		yMethod(x, length - 1);
+	}
+	
+	public static int zMethod(int n) {
+		if (n == 1)
+			return 1;
+		else
+			return n + zMethod(n - 1);
 	}
 
 }
