f7a63425967cc7bd16a00ee04e7491e1b5608175
James <145038163+SteelSinter@users.noreply.github.com>
2024-01-09 09:24:52 -0700

    Add files via upload

--- Minesweeper.java
+++ Minesweeper.java
@@ -7,10 +7,11 @@ import javafx.stage.Stage;
 public class Minesweeper extends Application {
 	final String BUTTON_STYLE = "-fx-border-radius: 0; -fx-border-color: #999999;"
 			+ " -fx-background-radius: 0; -fx-background-color: #ffffff;"/* -fx-text-fill: #999999*/;
+	final int MINES = 15;
+	int width = 10, height = 10, squareSize = 30;
 	
 	@Override
 	public void start(Stage mainStage) {
-		int width = 10, height = 10, squareSize = 30;
 		
 		Pane pane = new Pane();
 		
@@ -33,19 +34,38 @@ public class Minesweeper extends Application {
 	
 	class Board extends GridPane {
 		private int[][] matrix;
+		private boolean firstMove = true;
 		
 		Board(int width, int height) {
 			matrix = new int[width][height];
 			for (int c = 0; c < width; c++) {
 				for (int r = 0; r < height; r++) {
+					int col = c, row = r;
 					Button bt = new Button();
 					matrix[c][r] = 0;
 					bt.setMinSize(30, 30);
 					bt.setStyle(BUTTON_STYLE);
+					bt.setOnAction(e -> {
+						System.out.println("C:" + col + " " + "R:" + row);
+						if (firstMove)
+							addMines(MINES);
+					});
 					add(bt, c, r);
 				}
 			}
 			
+		}
+		
+		private void addMines(int mines) {
+			double mineChance = (double)mines / (width * height);
+			for (int c = 0; c < width; c++) {
+				for (int r = 0; r < height; r++) {
+					double rand = Math.random();
+					System.out.println(mineChance);
+					System.out.println(rand);
+				}
+			}
+			firstMove = false;
 		}
 	}
 
