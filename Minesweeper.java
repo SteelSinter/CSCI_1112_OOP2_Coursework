e3976b423f8c2590645573113a7d010038d09c7d
James <145038163+SteelSinter@users.noreply.github.com>
2024-01-09 10:46:00 -0700

    Add files via upload

--- Minesweeper.java
+++ Minesweeper.java
@@ -1,4 +1,5 @@
 import javafx.application.Application;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
@@ -8,7 +9,7 @@ public class Minesweeper extends Application {
 	final String BUTTON_STYLE = "-fx-border-radius: 0; -fx-border-color: #999999;"
 			+ " -fx-background-radius: 0; -fx-background-color: #ffffff;"/* -fx-text-fill: #999999*/;
 	final int MINES = 15;
-	int width = 10, height = 10, squareSize = 30;
+	int width = 10, height = 10, squareSize = 35;
 	
 	@Override
 	public void start(Stage mainStage) {
@@ -33,22 +34,25 @@ public class Minesweeper extends Application {
 	}
 	
 	class Board extends GridPane {
-		private int[][] matrix;
+		private boolean[][] matrix;
 		private boolean firstMove = true;
 		
 		Board(int width, int height) {
-			matrix = new int[width][height];
+			matrix = new boolean[width][height];
 			for (int c = 0; c < width; c++) {
 				for (int r = 0; r < height; r++) {
 					int col = c, row = r;
 					Button bt = new Button();
-					matrix[c][r] = 0;
-					bt.setMinSize(30, 30);
+					matrix[c][r] = false;
+					bt.setMinSize(squareSize, squareSize);
 					bt.setStyle(BUTTON_STYLE);
 					bt.setOnAction(e -> {
 						System.out.println("C:" + col + " " + "R:" + row);
-						if (firstMove)
-							addMines(MINES);
+						Button b = (Button)nodeAt(row, col);
+						System.out.println(b.getText());
+						if (firstMove) {
+							addMines(MINES, col, row);
+						}
 					});
 					add(bt, c, r);
 				}
@@ -56,16 +60,45 @@ public class Minesweeper extends Application {
 			
 		}
 		
-		private void addMines(int mines) {
-			double mineChance = (double)mines / (width * height);
-			for (int c = 0; c < width; c++) {
-				for (int r = 0; r < height; r++) {
-					double rand = Math.random();
-					System.out.println(mineChance);
-					System.out.println(rand);
+		private void addMines(int mines, int firstC, int firstR) {
+			int mineChance = (int)(((double)mines / (width * height)) * 1000);
+			//System.out.println(mineChance);
+			while (mines > 0) {
+				for (int c = 0; c < width; c++) {
+					for (int r = 0; r < height; r++) {
+						int rand = (int)(Math.random() * 1000);
+						//System.out.println(rand);
+						if (rand == mineChance && c != firstC && r != firstR) {
+							matrix[c][r] = true;
+							mines--;
+							System.out.println(mines);
+						}
+					}
 				}
 			}
 			firstMove = false;
+			revealMines(); // tmeporeoroperpoa
+		}
+		
+		public void revealMines() {
+			for (int c = 0; c < width; c++) {
+				for (int r = 0; r < height; r++) {
+					if (matrix[c][r]) {
+						Button bt = (Button)nodeAt(c, r);
+						bt.setText("M");
+						bt.setStyle(BUTTON_STYLE + ";-fx-text-fill: #ff0000");
+					}
+				}
+			}
+		}
+		
+		public Node nodeAt(int col, int row) {
+		    for (Node node : getChildren()) {
+		        if (GridPane.getColumnIndex(node) == col && GridPane.getRowIndex(node) == row) {
+		            return node;
+		        }
+		    }
+		    return null;
 		}
 	}
 
